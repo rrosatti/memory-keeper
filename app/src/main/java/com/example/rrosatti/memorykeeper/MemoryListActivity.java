25cbbf91e52738cba227868b1c39ffd12b310c62
Rodrigo R. Galvao <rodrigorosattig@gmail.com>
2017-08-26 19:14:48 -0300

    Create NewMemoryActivity
    The activity to add a new memory was created. At this point, only the layout is implemented.
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

--- app/src/main/java/com/example/rrosatti/memorykeeper/MemoryListActivity.java
+++ app/src/main/java/com/example/rrosatti/memorykeeper/MemoryListActivity.java
@@ -1,13 +1,32 @@
 package com.example.rrosatti.memorykeeper;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MemoryListActivity extends AppCompatActivity {
+
+    private FloatingActionButton btNewMemory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_memory_list);
+
+        iniViews();
+
+        btNewMemory.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent inNewMemory = new Intent(MemoryListActivity.this, NewMemoryActivity.class);
+                startActivity(inNewMemory);
+            }
+        });
+    }
+
+    private void iniViews() {
+        btNewMemory = (FloatingActionButton) findViewById(R.id.bt_new_memory);
     }
 }
---
+++ app/src/main/java/com/example/rrosatti/memorykeeper/NewMemoryActivity.java
@@ -0,0 +1,13 @@
+package com.example.rrosatti.memorykeeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewMemoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_memory);
+    }
+}
