412802ee12f5e1e99fa991a4bef7bfeaab48fcff
Rodrigo R. Galvao <rodrigorosattig@gmail.com>
2017-10-14 22:24:19 -0300

    Make MemoryListActivity class works
    The MemoryListActivity is now working. It makes a query from Firebase and get the memories of the logged user.
    
    Signed-off-by: Rodrigo R. Galvao <rodrigorosattig@gmail.com>

--- app/src/main/java/com/example/rrosatti/memorykeeper/activity/MemoryListActivity.java
+++ app/src/main/java/com/example/rrosatti/memorykeeper/activity/MemoryListActivity.java
@@ -4,25 +4,36 @@ import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.rrosatti.memorykeeper.R;
-import com.example.rrosatti.memorykeeper.model.User;
+import com.example.rrosatti.memorykeeper.adapter.MemoryAdapter;
+import com.example.rrosatti.memorykeeper.model.Memory;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemoryListActivity extends AppCompatActivity {
 
     private FloatingActionButton btNewMemory;
     private RecyclerView listOfMemories;
-    private FirebaseDatabase mDatabase;
+    private DatabaseReference database;
     private DatabaseReference userReference;
-    private FirebaseDatabase memoriesDatabase;
+    private DatabaseReference memoriesDatabase;
+    private String userId;
+    private MemoryAdapter memoryAdapter;
+    private List<Memory> memories;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,24 +42,42 @@ public class MemoryListActivity extends AppCompatActivity {
 
         iniViews();
 
-        final String userId = getIntent().getStringExtra("userId");
+        userId = getIntent().getStringExtra("userId");
 
-        mDatabase = FirebaseDatabase.getInstance();
-        userReference = mDatabase.getReference().child("users").child(userId);
+        database = FirebaseDatabase.getInstance().getReference();
+        userReference = database.child("users").child(userId);
 
-        ValueEventListener userListener = new ValueEventListener() {
+        memories = new ArrayList<>();
+
+        Query getMemories = database.child("memories").orderByChild("userId").equalTo(userId);
+        getMemories.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
-                Toast.makeText(getApplicationContext(), "Welcome " + user.getName(), Toast.LENGTH_SHORT).show();
+                if (dataSnapshot.exists()) {
+                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                        Memory memory = ds.getValue(Memory.class);
+                        memories.add(memory);
+                        memoryAdapter.add(memory);
+                    }
+                }
             }
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
+                Log.w("Warning", "Event canceled!");
             }
-        };
-        userReference.addValueEventListener(userListener);
+        });
+
+        memoryAdapter = new MemoryAdapter(this, new ArrayList<Memory>(0), new MemoryAdapter.MemoryListener() {
+            @Override
+            public void onMemoryClick(String memoryId) {
+                Toast.makeText(getApplicationContext(), "MemoryID: " + memoryId, Toast.LENGTH_SHORT).show();
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getApplicationContext(), 1);
+        listOfMemories.setLayoutManager(layoutManager);
+        listOfMemories.setItemAnimator(new DefaultItemAnimator());
+        listOfMemories.setAdapter(memoryAdapter);
 
         btNewMemory.setOnClickListener(new View.OnClickListener() {
             @Override
